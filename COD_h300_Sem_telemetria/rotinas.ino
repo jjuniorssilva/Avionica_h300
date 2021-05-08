81b2b5b13a4895ca238e08a708374014fe1ac63d
Deusimar Junior <junior-plya@live.com>
2021-05-08 11:58:36 -0300

    Add files via upload

---
+++ COD_h300_Sem_telemetria/rotinas.ino
@@ -0,0 +1,61 @@
+void rotinas(int op){
+  if(op==0){
+    if(millis()-ciclo>=1000){
+      if(check_I2C(0x77)==true && check_I2C(0x68)==true){ // realiza a checagem da conexão com os modulos
+        digitalWrite(led2, LOW);
+        if(status_calib==true){
+          packet_1.data.alt_inicial = calibracao_alt();
+          status_calib=false;
+        }
+      }else{
+        digitalWrite(led2, HIGH);
+      }
+      ciclo=millis();
+      rotinas(1);
+    }
+  }else if(op==1){
+    //--------coleta----------
+    if(check_I2C(0x77)==true && check_I2C(0x68)==true){ // realiza a checagem da conexão com os modulos
+      digitalWrite(led2, LOW);
+      getDataBMP(); // coleta dados do bmp
+      getDataMPU(); // coleta dados do mpu
+      getDataBatery(); // coleta dados da carga da bateria
+      if(check_direction){
+        if(check_height){
+          if(check_angle){
+            servo_control(179); // aciona o servo para liberar o paraquedas parametro:angulo
+            digitalWrite(led3, HIGH);
+          }
+        }
+      }
+      altura_ant= packet_1.data.alt_bmp; // guarda a altura coletada nesse clico para analisar no proximo
+    }else{
+      digitalWrite(led2, HIGH);
+    }
+    //--------armazenamento----------
+    if(check_EEPROM()){ //verifica o armazenamento
+      sendDataEEPROM();//Salvar os dados do struct
+      digitalWrite(led2, LOW);
+    }else{
+      digitalWrite(led2, HIGH);
+    }
+    
+  }else{
+    for(int x=0;x<180;x++){
+      //converte graus para radiando e depois obtém o valor do seno
+      float seno=(sin(x*3.1416/180));
+      //gera uma frequência a partir do valor do seno
+      int frequencia = 2000+(int(seno*1000));
+      tone(9,frequencia);
+      delay(2);
+    }   
+  }
+}
+void update_rotina(){ // a ideia aqui é que >=1m de altura o modo de voo mude e ao retornar a <=1m de altura mude de novo;
+  if(packet_1.data.alt_bmp>=1 && status_alt_rotina==false){
+    status_alt_rotina=true;
+    rotina=1;
+  }else if(packet_1.data.alt_bmp<=1 && status_alt_rotina==true){
+    rotina=2;
+  }
+}
---
+++ COD_h300_Sem_telemetria/sens_arm_trans.ino
@@ -0,0 +1,104 @@
+ //----------bmp280-------------------
+float calibracao_alt(){
+ float alt0=0,alt1=0,alt2=0,alt3=0,media_alt,percent=0.05;
+ alt0=bmp.readAltitude();
+ alt1=bmp.readAltitude();
+ alt2=bmp.readAltitude(); 
+ alt3=bmp.readAltitude();
+ media_alt = (alt1+alt2+alt3)/3;
+ if(alt0>=(media_alt+(media_alt*percent)) || alt0<=(media_alt-(media_alt*percent))){ // verifica se o valor coletado está dentro do intervalo gerado pela média
+    return media_alt;
+ }else{
+    return alt0;
+ }
+}
+void getDataBMP(){
+  float alt0=0,alt1=0,alt2=0,alt3=0,media_alt,percent=0.05;
+  packet_1.data.pressao_bmp = bmp.readPressure();
+  alt0=bmp.readAltitude();
+  alt1=bmp.readAltitude();
+  alt2=bmp.readAltitude(); 
+  alt3=bmp.readAltitude();
+  media_alt = (alt1+alt2+alt3)/3;
+  if(alt0>=(media_alt+(media_alt*percent)) || alt0<=(media_alt-(media_alt*percent))){ // verifica se o valor coletado está dentro do intervalo gerado pela média
+    packet_1.data.alt_bmp = media_alt - packet_1.data.alt_inicial;
+  }else{
+    packet_1.data.alt_bmp =alt0 - packet_1.data.alt_inicial;
+  }
+}
+// ----checagem da conexão i2c------
+bool check_I2C(uint8_t ADDRESS){
+  Wire.beginTransmission(ADDRESS);
+  byte erro = Wire.endTransmission();
+  if(erro==0)return true;
+  return false;
+}
+//----------MPU6050-------------------
+void getDataMPU(){
+  Wire.beginTransmission(0x68);
+  Wire.write(0x3B);  // starting with register 0x3B (ACCEL_XOUT_H)
+  Wire.endTransmission(false);
+  //Solicita os dados do sensor
+  Wire.requestFrom(0x68,14,true);  
+  //Armazena o valor dos sensores nas variaveis correspondentes
+  packet_1.data.AcX_mpu=Wire.read()<<8|Wire.read();  //0x3B (ACCEL_XOUT_H) & 0x3C (ACCEL_XOUT_L)     
+  packet_1.data.AcY_mpu=Wire.read()<<8|Wire.read();  //0x3D (ACCEL_YOUT_H) & 0x3E (ACCEL_YOUT_L)
+  packet_1.data.AcZ_mpu=Wire.read()<<8|Wire.read();  //0x3F (ACCEL_ZOUT_H) & 0x40 (ACCEL_ZOUT_L)
+  packet_1.data.Tmp_mpu=Wire.read()<<8|Wire.read();  //0x41 (TEMP_OUT_H) & 0x42 (TEMP_OUT_L)
+  packet_1.data.GyX_mpu=Wire.read()<<8|Wire.read();  //0x43 (GYRO_XOUT_H) & 0x44 (GYRO_XOUT_L)
+  packet_1.data.GyY_mpu=Wire.read()<<8|Wire.read();  //0x45 (GYRO_YOUT_H) & 0x46 (GYRO_YOUT_L)
+  packet_1.data.GyZ_mpu=Wire.read()<<8|Wire.read();  //0x47 (GYRO_ZOUT_H) & 0x48 (GYRO_ZOUT_L)
+}
+//----------EEPROM-------------------
+void writeEEPROM(int deviceaddress, unsigned int eeaddress, byte data ) { // salva o dado (byte) na posição eeaddress
+  Wire.beginTransmission(deviceaddress);
+  Wire.write((int)(eeaddress >> 8));   // MSB
+  Wire.write((int)(eeaddress & 0xFF)); // LSB
+  Wire.write(data);
+  Wire.endTransmission();
+  delay(4);
+}
+byte readEEPROM(int deviceaddress, unsigned int eeaddress ) {
+  byte data = 0xFF;
+  Wire.beginTransmission(deviceaddress);
+  Wire.write((int)(eeaddress >> 8));   // MSB
+  Wire.write((int)(eeaddress & 0xFF)); // LSB
+  Wire.endTransmission();
+  Wire.requestFrom(deviceaddress,1);
+  if (Wire.available()) data = Wire.read();
+  return data;
+}
+void sendDataEEPROM(){ // consumo médio de tempo 160ms
+ for(int i=0;i<40;i++){ // no struct, os dados relevantes então no array de bytes [1-40]
+  if(address_eeprom.eeaddress<65536){ // impede de gravar dados em um endereço inexistente 
+    writeEEPROM(0x50,address_eeprom.eeaddress,packet_1.data_byte[i]);
+    address_eeprom.eeaddress++;
+    send_backup_address();
+  }else{
+    Serial.println("Memoria eeprom cheia! -> ultimo endereço: "+String(address_eeprom.eeaddress));
+  }
+ }
+}
+bool check_EEPROM(){ // Grava um byte na memoria e faz a leitura, se for igual passa.
+  if(address_eeprom.eeaddress<65536){ // impede de gravar dados em um endereço inexistente 
+    writeEEPROM(address_eeprom.eeaddress,address_eeprom.eeaddress,0x50);
+    if(readEEPROM(address_eeprom.eeaddress,address_eeprom.eeaddress)==0x50) return true;
+  }
+  return false;
+}
+void send_backup_address(){
+  for(int i=0;i<4;i++){
+    EEPROM.write(i, address_eeprom.eeaddress_byte[i]);
+  }
+}
+void get_backup_address(){
+  for(int i=0;i<4;i++){
+    address_eeprom.eeaddress_byte[i] = EEPROM.read(i);
+  }
+}
+// ------------Bateria-------
+float getDataBatery(){
+  int val = analogRead(pin_batery);
+  packet_1.data.batery = map(val, 0, 1023, 0, 100);// precisa ser adaptado
+  return packet_1.data.batery;
+}
